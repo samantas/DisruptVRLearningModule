a9196d69dae3182ed13aa61e84b6459b7be2d8ba
Sean Kelly <sgk291@nyu.edu>
2016-05-07 23:44:57 -0400

    sean final touches

---
+++ Assets/Carnival/Samples/FingertipSample/Scripts/MoleBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class MoleBehaviour : MonoBehaviour
+{
+	public float _initY;
+
+	public AudioClip impact;
+	AudioSource audio;
+
+
+    void Start()
+    {
+        // Remember initial position
+        _initY = transform.position.y;
+
+		audio = GetComponent<AudioSource>();
+		//gameObject.GetComponent<Renderer> ().material.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Tag mole object according to its position
+
+        // Arrived its initial position?
+        if (transform.position.y >= _initY)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            gameObject.tag = "aboveGround";
+
+			//test this
+			//Debug.Log("going underground");
+        }
+
+//        // Underneath ground?
+//		if (GetComponent<BoxCollider>().bounds.max.y < 0)
+//        {
+//            gameObject.tag = "underGround";
+//        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+		
+        if (gameObject.tag == "aboveGround")
+        {
+			audio.PlayOneShot(impact, 0.7F);
+
+
+
+			//test color changing on collider enter
+			gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+
+            // Mole falls down when it is hit by collider
+            GetComponent<Rigidbody>().useGravity = true;
+            gameObject.tag = "movingDown";
+
+			gameObject.tag = "underGround";
+
+        }
+    }
+
+}
